d4ef8d6157d1ffc69b241c744e1858a9e6d4ed66
D3coru5 <chpeterle.profissional@gmail.com>
2021-08-01 11:23:06 -0300

    Homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,78 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Box, Grid } from '@material-ui/core';
 
-import Layout from '../components/Layout';
+import VideoCard from 'src/components/VideoCard';
+import Layout from 'src/components/Layout';
 
-export default function Home() {
+function Home({ data }) {
   return (
     <Layout title="Youtube">
-      clone youtube
-      <Button variant="contained" color="secondary">
-        Hello world
-      </Button>
+      <Box p={2}>
+        <Grid container spacing={4}>
+          {data.map((item) => (
+            <Grid item key={item.id} xl={3} lg={3} md={4} sm={6} xs={12}>
+              <VideoCard item={item} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Layout>
   );
 }
+
+export async function getStaticProps() {
+  const data = [
+    {
+      id: 1,
+      title: 'NEXT.JS: O FRAMEWORK QUE VOCÊ DEVERIA CONHECER [PARTE #01]',
+      authorId: '',
+      authorName: 'Lucas Nhimi',
+      authorAvater: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next01.png',
+      videoUrl: 'url',
+      updateAt: new Date(),
+    },
+    {
+      id: 2,
+      title:
+        'NEXT.JS: ENTENDENDO A RENDERIZAÇÃO REACT NO LADO SERVIDOR [PARTE #02]',
+      authorId: '',
+      authorName: 'Lucas Nhimi',
+      authorAvater: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next02.png',
+      videoUrl: 'url',
+      updateAt: new Date(),
+    },
+    {
+      id: 3,
+      title: 'NEXT.JS: ROTAS ESTÁTICAS E DINÂMICAS [PARTE #03]',
+      authorId: '',
+      authorName: 'Lucas Nhimi',
+      authorAvater: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next03.png',
+      videoUrl: 'url',
+      updateAt: new Date(),
+    },
+    {
+      id: 4,
+      title: 'NEXT.JS: ROTAS ESTÁTICAS E DINÂMICAS [PARTE #03]',
+      authorId: '',
+      authorName: 'Lucas Nhimi',
+      authorAvater: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next03.png',
+      videoUrl: 'url',
+      updateAt: new Date(),
+    },
+  ];
+  return {
+    props: {
+      data: JSON.parse(JSON.stringify(data)),
+    },
+  };
+}
+
+export default Home;
